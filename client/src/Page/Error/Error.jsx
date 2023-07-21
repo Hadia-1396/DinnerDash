d41ce67675221c9ebf66748f8697f390ec43d4b7
hadiashabbir <hadiashabbir4@gmail.com>
2023-07-21 21:24:49 +0500

    order

---
+++ client/src/Page/Error/Error.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Error = () => {
+  return (
+    <div className="container text-center mt-5">
+      <h1>You are not logged in!</h1>
+      <p className="mt-5">Please log in to checkout</p>
+    </div>
+  );
+};
+
+export default Error;
